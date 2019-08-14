0813ac57d9d6163f33e5bdc36f0523a76d1b02e1
ha191180 <matuakr1212@gmail.com>
2019-08-14 20:41:09 +0900

    8/14

---
+++ ABC137A-sel.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main(){
+	int a,b,num1,num2,num3,max=-100000;
+	cin >> a >> b;
+	num1 = a+b;
+	num2 = a-b;
+	num3 = a*b;
+	if (num1>max)max = num1;
+	if (num2>max)max = num2;
+	if (num3>max)max = num3;
+	cout << max << endl;
+	return 0;
+	}
