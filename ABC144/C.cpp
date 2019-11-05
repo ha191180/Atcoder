a1507177be8249892b605e29904fe8aee3f8393d
ha191180 <matuakr1212@gmail.com>
2019-11-05 16:24:04 +0900

    add144

---
+++ ABC144/C.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+	int n, start,x,y;
+	cin >> n ;
+	if(n % 2 == 0){
+		start = n / 2;
+	}else{
+		start = (n + 1) / 2;
+	}
+	for(int i = start ; i < n ; i++){
+		if(n % i == 0){
+			x = i;
+			y = n / i;
+			i = n;
+		}
+	}
+	cout << x + y -2 << endl;
+	return 0;
+}
